9623951b391af831388ff38054d0752e647bb687
GoyalRitvik <125717243+GoyalRitvik@users.noreply.github.com>
2023-10-01 16:08:04 -0400

    Add files via upload

---
+++ script (2).js
@@ -0,0 +1,20 @@
+const btn = document.getElementById('enter');
+const inp = document.getElementById('input'); 
+const addto = document.getElementById('tasks');
+btn.addEventListener('click', () => {
+  const x = inp.value;
+  if (x == '') {
+    alert("Blank Field... Please write a task before submitting!");
+  }
+  else{
+  const task = document.createElement('p');
+  task.textContent = x;
+  const crossout = document.createElement('button');
+  crossout.textContent = "x";
+  crossout.addEventListener('click', () => {
+    addto.removeChild(task);
+  });
+  addto.appendChild(task);
+  task.appendChild(crossout);
+  inp.value = '';
+  };})
